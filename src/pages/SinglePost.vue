d2e3f9bc9d42771f3c57fb23c0524a244e0818f2
Leonart Gutz <leonartgutz@hotmail.com>
2020-03-08 03:45:18 -0700

    New file structure, layout changes, layout fixies

---
+++ src/pages/SinglePost.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-page class="post-list">
+
+    <!-- Post Item -->
+    <div class="post-list-item">
+
+      <!-- Post Header -->
+      <header>
+
+        <!-- Post Item Avatar -->
+        <q-btn
+        to="/UserProfile"
+        :ripple="false"
+        >
+
+          <q-avatar
+          size="48px"
+          >
+
+            <q-img
+            src="https://images.pexels.com/photos/850602/pexels-photo-850602.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            :ratio="1"
+            />
+
+          </q-avatar>
+
+        </q-btn>
+        <!-- /Post Item Avatar -->
+
+        <!-- Post Item Info -->
+        <div class="post-list-info">
+          <q-item
+          clickable
+          to="/UserProfile"
+          >
+            <q-item-section>
+              <q-item-label overline>Charlie</q-item-label>
+              <q-item-label caption>Stanley Park</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+        <!-- /Post Item Info -->
+
+      </header>
+      <!-- /Post Header -->
+
+      <!-- Post Item Title -->
+      <div class="post-list-title">
+        <h1>Hello, Buddies! Here I'm enjoying the summer. It would be great if you could be here with me!</h1>
+      </div>
+      <!-- /Post Item Title -->
+
+      <!-- Post Item Pic -->
+      <q-img
+      src="https://images.pexels.com/photos/850602/pexels-photo-850602.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+      ratio="1"
+      />
+      <!-- /Post Item Pic -->
+
+      <!-- Post Item Btns -->
+      <div class="post-list-social">
+        <q-btn
+        class="btn-social"
+        >
+          <img src="../assets/layout/paw-icon.svg">
+        </q-btn>
+        <p class="counter">00</p>
+      </div>
+      <!-- /Post Item Btns -->
+
+      <!-- Post Item Comments -->
+      <div class="post-list-comments-wrapper">
+        <ul>
+          <li>
+            <span class="name">Charlie:</span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae repellat blanditiis tenetur esse, pariatur asperiores similique repellendus, quidem doloremque praesentium maxime molestias!
+            <br>
+            <span class="date">Aug 1, 2020 00:00</span>
+          </li>
+          <li>
+            <span class="name">Charlie:</span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae repellat blanditiis tenetur esse, pariatur asperiores similique repellendus, quidem doloremque praesentium maxime molestias!
+            <br>
+            <span class="date">Aug 1, 2020 00:00</span>
+          </li>
+        </ul>
+      </div>
+      <!-- /Post Item Comments -->
+
+    </div>
+    <!-- /Post Item -->
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'SinglePost',
+}
+</script>
---
+++ src/pages/UserProfile.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-page class="user-profile">
+    
+    <!-- User Info -->
+    <div class="user-profile-info">
+
+      <!-- User Avatar -->
+      <q-avatar
+      size="72px"
+      >
+
+        <q-img
+        src="https://images.pexels.com/photos/850602/pexels-photo-850602.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+        :ratio="1"
+        />
+
+      </q-avatar>
+      <!-- /User Avatar -->
+
+      <!-- User Name & Info -->
+      <div class="user-profile-name">
+        <q-item>
+          <q-item-section>
+            <q-item-label overline>Charlie</q-item-label>
+            <q-item-label caption>000 Followers &bull; 000 Following</q-item-label>
+
+            <q-list>
+
+              <q-item
+              clickable
+              to="/AboutMe"
+              class="btn-text"
+              >
+                <q-item-label>
+                  About Me
+                </q-item-label>
+              </q-item>
+
+              &bull;
+
+              <q-item
+              clickable
+              to="/LINK-HERE"
+              class="btn-text"
+              >
+                <q-item-label>
+                  Edit Profile
+                </q-item-label>
+              </q-item>
+
+            </q-list>
+
+          </q-item-section>
+        </q-item>
+      </div>
+      <!-- User Name & Info -->
+
+    </div>
+    <!-- /User Info -->
+
+    <!-- Btn Follow Area -->
+    <div class="global-separator">
+      <q-btn
+      class="btn-small btn-purple btn-round"
+      >
+      Follow
+      </q-btn>
+    </div>
+    <!-- /Btn Follow Area -->
+
+    <!-- Picture Tiles -->
+    <div class="picture-tiles-wrapper">
+      <q-btn
+      class="btn-picture"
+      to="/SinglePost"
+      >
+        <q-img
+        src="https://images.pexels.com/photos/850602/pexels-photo-850602.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+        :ratio="1"
+        />
+      </q-btn>
+    </div>
+    <!-- /Picture Tiles -->
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+}
+</script>
